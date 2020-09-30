3fa1ff837a6bfc680e160fb9b9c04c25682efd5a
taibislamdipu <mailtaibislam@gmail.com>
2020-10-01 05:13:01 +0600

    ema john with auth added new config and initial repo setup done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDge6LHw4uUW6J2_CsRSe-9ik6PrOcoL1Y",
+  authDomain: "ema-john-with-auth-d0bd8.firebaseapp.com",
+  databaseURL: "https://ema-john-with-auth-d0bd8.firebaseio.com",
+  projectId: "ema-john-with-auth-d0bd8",
+  storageBucket: "ema-john-with-auth-d0bd8.appspot.com",
+  messagingSenderId: "566454790944",
+  appId: "1:566454790944:web:e2a70c49036857a85bc545"
+};
+export default firebaseConfig;
